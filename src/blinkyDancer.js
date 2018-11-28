ef344820ddd6da8c6dc53ca5869ee8d5bbf9dd54
gizmo <gizmo@Gremlin.local>
2018-11-27 22:39:14 -0600

    changed image file

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,41 +1,24 @@
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-//   var blinkyDancer = new BlinkyDancer();
-//   return blinkyDancer;
-// }
+var BlinkyDancer = function (top, left, timeBetweenSteps) {
 
-var BlinkyDancer = function(top,left,timeBetweenSteps) {
-  // this.top = top;
-  // this.left = left;
-  // this.timeBetweenSteps = timeBetweenSteps;
-  Dancer.call(this,top,left,timeBetweenSteps);
-  //this.step(timeBetweenSteps);
-  //this.setPosition(top, left);
+  Dancer.call(this, top, left, timeBetweenSteps);
+
 }
 
-  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+// class BlinkyDancer extends Dancer {
+//   constructor(top, left, timeBetweenSteps) {
+//     super(top, left, timeBetweenSteps);
+//   }
+//   //Dancer.call(this, top, left, timeBetweenSteps)
+// }
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
- 
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-  BlinkyDancer.prototype = Object.create(Dancer.prototype);
-  BlinkyDancer.prototype.constructor = BlinkyDancer;
+var oldStep = Dancer.prototype.step;
 
-  var oldStep = Dancer.prototype.step;
+BlinkyDancer.prototype.step = function (timeBetweenSteps) {
 
-  BlinkyDancer.prototype.step = function(timeBetweenSteps) {
-    // call the old version of step at the beginning of any call to this new version of step
-    //console.log('argh')
-    oldStep.call(this,timeBetweenSteps);
-
-    //oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    //console.log('this.node = ', this.$node)
-    this.$node.toggle();
-  };
-
-  //return blinkyDancer;
-//};
+  oldStep.call(this, timeBetweenSteps);
+  this.$node.toggle();
+};
 
--- src/clownDancer.js
+++ src/clownDancer.js
@@ -1,17 +1,19 @@
-var ClownDancer = function(top, left, timeBetweenSteps) {
+var ClownDancer = function (top, left, timeBetweenSteps) {
     Dancer.call(this, top, left, timeBetweenSteps);
-    this.$node.addClass('clown-dancer').append('<img src="http://www.pngmart.com/files/3/Creepy-Transparent-Background.png">');
+    // this.$node.addClass('clown-dancer').append('<img src="http://www.pngmart.com/files/3/Creepy-Transparent-Background.png">');
+    this.$node.addClass('clown-dancer').append('<img src="./images/panda.png">');
+
 }
 
 ClownDancer.prototype = Object.create(Dancer.prototype);
 ClownDancer.prototype.constructor = ClownDancer;
 
-ClownDancer.prototype.step = function(timeBetweenSteps) {
+ClownDancer.prototype.step = function (timeBetweenSteps) {
     Dancer.prototype.step.call(this, timeBetweenSteps);
     //$('span').append('<img src="...."')
     //console.log($(this))
     //console.log(this.$node)
-    $('.clown-dancer').toggle( "bounce", { times: 3 }, "slow" )
+    $('.clown-dancer').toggle("bounce", { times: 3 }, "slow")
     //this.$node.effect('shake');   
 };
 
